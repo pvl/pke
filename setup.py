3b29513539740751453e452067aedf83632a9412
Pedro Lima <pedro.lima@gmail.com>
2023-01-04 16:05:22 +0000

    also update setup.py

--- setup.py
+++ setup.py
@@ -11,16 +11,16 @@ setup(name='pke',
                 'pke.unsupervised.statistical', 'pke.supervised.neural_based'],
       url="https://github.com/boudinfl/pke",
       install_requires=[
-          'nltk',
-          'networkx',
-          'numpy',
-          'scipy',
-          'spacy',
-          'six',
-          'sklearn',
-          'unidecode',
-          'future',
-          'joblib'
+            "six==1.14.0",
+            "spacy==2.2.4",
+            "nltk==3.4",
+            "networkx==2.4",
+            "numpy==1.19.2",
+            "scipy==1.4.1",
+            "scikit-learn==0.23.1",
+            "unidecode==1.3.4",
+            "future==0.18.2",
+            "joblib==0.14.1"
       ],
       package_data={'pke': ['models/*.pickle', 'models/*.gz']}
       )
